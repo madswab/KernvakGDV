ad740118cf1eb9094d0ab618ca911ddd60fee19d
madswab <31699111+madswab@users.noreply.github.com>
2017-10-26 23:54:11 +0200

    Add files via upload

---
+++ PacmanFinal/3DPacmanGDVFinal/Assets/Daan/Animations/Characters/PACMAN/PACMAN_Animation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PACMAN_Animation : MonoBehaviour {
+
+    [SerializeField] private Material pacmanMaterial;
+    [SerializeField]
+    private Texture[] sprites;
+
+    [SerializeField] private float framesPerSecond = 1;
+
+    private int currentSprite = 0;
+    float counter = 0;
+
+    private void Awake() {
+        pacmanMaterial.SetTexture("_MainTex", sprites[currentSprite]);
+        pacmanMaterial.SetTexture("_EmissionMap", sprites[currentSprite]);
+    }
+
+    private void FixedUpdate() {
+        counter += Time.fixedDeltaTime;
+        if(counter >= 1f/framesPerSecond) {
+            counter = 0;
+            
+            currentSprite++;
+            if(currentSprite >= sprites.Length) {
+                currentSprite = 0;
+            }
+
+            pacmanMaterial.SetTexture("_MainTex", sprites[currentSprite]);
+            pacmanMaterial.SetTexture("_EmissionMap", sprites[currentSprite]);
+        }
+    }
+	
+}
